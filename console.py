40c8eb941bbdb119d1dd6fcbd1116b9a8300ea01
Getinet <getinetamare@gmail.com>
2023-02-11 22:13:50 +0300

    Create console.py

--- console.py
+++ console.py
@@ -42,11 +42,11 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb) '
 
-    def do_quit(self, arg):
+    def quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, arg):
+    def EOF(self, arg):
         """EOF command to exit the program"""
         return True
 
